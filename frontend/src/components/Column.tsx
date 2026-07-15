0f90fb99c601e657c19cfe2f0d7243749cb352b5
Ezgi Nur Yigit <ezgi@E-yLAPTOP.localdomain>
2026-07-15 16:19:37 +0300

    chore(frontend): add Prettier config and apply formatting

--- frontend/src/components/Column.tsx
+++ frontend/src/components/Column.tsx
@@ -2,20 +2,20 @@ import type { Task } from "../types/Task";
 import TaskCard from "./TaskCard";
 
 type ColumnProps = {
-    title: string;
-    tasks: Task[];
-}
+  title: string;
+  tasks: Task[];
+};
 
 function Column(props: ColumnProps) {
-    return(
-        <div>
-            <h2>{props.title}</h2>
+  return (
+    <div>
+      <h2>{props.title}</h2>
 
-            {props.tasks.map((task) => (
-                <TaskCard task={task} key={task.id} />
-            ))}
-        </div>
-    );
+      {props.tasks.map((task) => (
+        <TaskCard task={task} key={task.id} />
+      ))}
+    </div>
+  );
 }
 
 export default Column;
--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -1,18 +1,18 @@
-import type { Task } from '../types/Task';
+import type { Task } from "../types/Task";
 
 type TaskCardProps = {
   task: Task;
 };
 
 function TaskCard(props: TaskCardProps) {
-    return(
-        <div>
-            <h3>{props.task.title}</h3>
-            <p>{props.task.description}</p>
-            <span>{props.task.id}</span>
-            <span>{props.task.priority}</span>
-        </div>
-    );
+  return (
+    <div>
+      <h3>{props.task.title}</h3>
+      <p>{props.task.description}</p>
+      <span>{props.task.id}</span>
+      <span>{props.task.priority}</span>
+    </div>
+  );
 }
 
 export default TaskCard;
