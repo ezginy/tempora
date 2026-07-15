788049e84cb25a99faf67dc39962cf0eed6d6cbd
Ezgi Nur Yigit <ezgi@E-yLAPTOP.localdomain>
2026-07-15 07:27:02 +0300

    feat(frontend): add Task type and static TaskCard component
    - Define Task and Priority types matching backend API contract
    - Build TaskCard component rendering title, description, priority, and id

---
+++ frontend/src/components/TaskCard.tsx
@@ -0,0 +1,18 @@
+import type { Task } from '../types/Task';
+
+type TaskCardProps = {
+  task: Task;
+};
+
+function TaskCard(props: TaskCardProps) {
+    return(
+        <div>
+            <h3>{props.task.title}</h3>
+            <p>{props.task.description}</p>
+            <span>{props.task.id}</span>
+            <span>{props.task.priority}</span>
+        </div>
+    );
+}
+
+export default TaskCard;
